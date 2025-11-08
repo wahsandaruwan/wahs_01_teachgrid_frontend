328e75d8c935e63e45cb04e1cfa81adfcce96db2
Safwan <140247051+Mrghost077@users.noreply.github.com>
2025-11-08 09:03:29 +0530

    Revert "Fixed mirror error bug"

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import SignInPage from "./pages/SignInPage";
-import TeacherDashboard from "./pages/TeacherDashboard";
-import AdminDashboard from "./pages/AdminDashboard";
+import AdminDashboard from "./AdminDashboard";
+import Attendance from "./Attendance";
 
-
-const ProtectedRoute = ({ children, role }) => {
-  const userRole = localStorage.getItem("role");
-  const token = localStorage.getItem("token");
-
-  if (!token) {
-    
-    return <Navigate to="/SignIn" />;
-  }
-
-  if (role && userRole !== role) {
-    
-    return <Navigate to="/SignIn" />;
-  }
-
-  return children;
-};
-
-function App() {
+function App () {
   return (
-    <BrowserRouter>
-      <Routes>
-       
-        <Route path="/" element={<Navigate to="/SignIn" />} />
-
-        
-        <Route path="/SignIn" element={<SignInPage />} />
-
-        
-        <Route
-          path="/teacher-dashboard"
-          element={
-            <ProtectedRoute role="teacher">
-              <TeacherDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-       
-        <Route
-          path="/admin-dashboard"
-          element={
-            <ProtectedRoute role="admin">
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        
-        <Route path="*" element={<Navigate to="/SignIn" />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <div>
+      
+      <AdminDashboard/>
+      {/* <Attendance/> */}
+    </div>
+  )
 }
 
 export default App;
